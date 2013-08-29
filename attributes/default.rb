5cd0111379e8b2892575ea9dc40fbc7106170fba
Blake Irvin <bixu@wanelo.com>
2013-08-29 11:48:27 -0700

    Skeleton cookbook

---
+++ attributes/default.rb
@@ -0,0 +1,3 @@
+default['paths']['lib_path'] = "/lib:/usr/lib:/opt/local/lib:/opt/gcc/lib"
+default['paths']['bin_path'] = "/opt/local/bin:/opt/local/sbin:/usr/bin:/usr/sbin"
+default['paths']['etc_path'] = "/opt/local/etc/"
