5806a43cbf9f46900ff51d461796981e1a21f907
Blake Irvin & Eric Saxby <pair+blake+sax@wanelo.com>
2013-09-24 16:01:23 -0700

    Correctly target sngl, add bin_dir attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,16 +1,22 @@
 case platform
 when "smartos"
-  if `modinfo | grep sngl 2>&1 > /dev/null`
+  if system("modinfo | grep sngl 2>&1 > /dev/null")
     default['paths']['lib_path'] = "/system/lib:/system/usr/lib:/usr/local/lib"
     default['paths']['bin_path'] = "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin"
+    default['paths']['bin_dir']  = "/usr/local/bin"
   else
     default['paths']['lib_path'] = "/lib:/usr/lib:/opt/local/lib:/opt/gcc/lib"
     default['paths']['bin_path'] = "/opt/local/bin:/opt/local/sbin:/usr/bin:/usr/sbin"
-    default['paths']['etc_path'] = "/opt/local/etc"
+    default['paths']['bin_dir']  = "/opt/local/bin"
+    default['paths']['etc_dir']  = "/opt/local/etc"
   end
 when "ubuntu", "debian"
   default['paths']['lib_path'] = "/usr/local/lib:/lib/x86_64-linux-gnu:/usr/lib/x86_64-linux-gnu"
   default['paths']['bin_path'] = "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+  default['paths']['bin_dir']  = "/usr/local/bin"
 else
-  default['paths']['etc_path'] = "/etc"
+  default['paths']['lib_path'] = "/usr/local/lib"
+  default['paths']['bin_path'] = "/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin"
+  default['paths']['bin_dir']  = "/usr/local/bin"
+  default['paths']['etc_dir']  = "/etc"
 end
